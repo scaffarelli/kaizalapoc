324e6f10dbdb8e5d498aa5aa5922a25e9ed7b3b7
Sebastian Caffarelli <scaffarelli@gmail.com>
2019-10-03 19:11:40 -0300

    Initial

---
+++ app.js
@@ -0,0 +1,35 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+const https = require("https")
+const data = JSON.stringify({ "name": "Sebastian" })
+
+const options = {
+  hostname: "env7rk1tvl46b.x.pipedream.net",
+  port: 443,
+  path: "/",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": data.length,
+  },
+}
+
+const req = https.request(options)
+req.write(data)
+req.end()
+
+app.get('/', function (req, res) {
+  res.send('Webhook - POC!');
+});
+
+module.exports = app;
