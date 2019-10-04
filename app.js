3d296232761d6a90439e55d274e5862956b73988
Sebastian Caffarelli <scaffarelli@gmail.com>
2019-10-04 14:27:54 -0300

    validationToken

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 
 const https = require("https")
 const data = JSON.stringify({ "name": "Sebastian" })
 
-const options = {
-  hostname: "env7rk1tvl46b.x.pipedream.net",
-  port: 443,
-  path: "/",
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Content-Length": data.length,
-  },
-}
-
-const req = https.request(options)
-req.write(data)
-req.end()
 
 app.get('/', function (req, res) {
-  res.send(req.query.validationToken);
+
+  //req.query.validationToken.toString()
+  let token =  req.query.validationToken
+  const dataHook = JSON.stringify( token )
+
+
+  const options = {
+    hostname: "env7rk1tvl46b.x.pipedream.net",
+    port: 443,
+    path: "/",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": dataHook.length,
+    },
+  }
+
+  const reqHook = https.request(options)
+
+  reqHook.write(dataHook.toString())
+  reqHook.end()
+
+  res.send(req.query);
 });
 
 module.exports = app;
